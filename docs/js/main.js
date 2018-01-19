2188a7d70b136aee59fda169680a7342d3e5b837
Harri Tapio <harritapio@Harris-MacBook-Pro.local>
2018-01-19 14:50:42 +0200

    Added NSCA

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,12 @@ squatCoefficients    = [1, 1.0475, 1.130, 1.1575, 1.200, 1.242, 1.284, 1.326, 1.
 benchCoefficients    = [1, 1.0350, 1.080, 1.1150, 1.150, 1.180, 1.220, 1.255, 1.290, 1.325];
 deadliftCoefficients = [1, 1.0650, 1.130, 1.1470, 1.164, 1.181, 1.198, 1.232, 1.232, 1.240];
 
+nscaCoefficients = {
+  squat:    [1, 1.0650, 1.130, 1.1470, 1.164, 1.181, 1.198, 1.232, 1.232, 1.240],
+  bench:    [1, 1.0475, 1.130, 1.1575, 1.200, 1.242, 1.284, 1.326, 1.368, 1.410],
+  deadlift: [1, 1.0350, 1.080, 1.1150, 1.150, 1.180, 1.220, 1.255, 1.290, 1.325]
+}
+
 function get1RM(weight, reps, method) {
 
   var rm1 = 0;
@@ -19,15 +25,15 @@ function get1RM(weight, reps, method) {
     rm1= weight / ( 1.013 - ( 0.0267123 * reps ) );
   }
 
-  else if ( method == "nscasquat" ) {
+  else if ( method == "Squat" ) {
     rm1= weight * squatCoefficients[reps-1];
   }
 
-  else if ( method == "nscabench" ) {
-    rm1= weight * benchCoefficients[reps-1];
+  else if ( method == "Bench" ) {
+    rm1= weight * nscaCoefficients.bench[reps-1];
   }
 
-  else if ( method == "nscadeadlift" ) {
+  else if ( method == "Deadlift" ) {
     rm1= weight * deadliftCoefficients[reps-1];
   }
 
@@ -49,66 +55,70 @@ function calcCoefficients(method) {
   return(coefficients);
 }
 
-var percents = [100,95,90,85,80,75,50,65,60,50];
+var percents = [100,95,90,85,80,75,70,65,60,50];
 
 var repsdata = {
   data: {
-    reps: [
-      [1,  0, 0, 0],
-      [2,  0, 0, 0],
-      [3,  0, 0, 0],
-      [4,  0, 0, 0],
-      [5,  0, 0, 0],
-      [6,  0, 0, 0],
-      [7,  0, 0, 0],
-      [8,  0, 0, 0],
-      [9,  0, 0, 0],
-      [10, 0, 0, 0]
-    ],
-    percentages: [
-      ['100%', 0,0,0],
-      ['95%',  0,0,0],
-      ['90%',  0,0,0],
-      ['85%',  0,0,0],
-      ['80%',  0,0,0],
-      ['75%',  0,0,0],
-      ['70%',  0,0,0],
-      ['65%',  0,0,0],
-      ['60%',  0,0,0],
-      ['50%',  0,0,0]
-    ]
+    replabels: ['Reps', 'Est A', 'Est B', 'Est C'],
+    percentlabels: ['Percent', 'Est A', 'Est B', 'Est C'],
+    reps: [],
+    percentages: []
   }
 };
 
-function updateData(weight, reps) {
+var nsca = 'No NSCA calculation';
+
+function updateData(weight, reps, nsca) {
   var brzycki_c = calcCoefficients("brzycki");
   var epley_c = calcCoefficients("epley");
   var lander_c = calcCoefficients("lander");
   var brzycki_1rm = get1RM(weight, reps, "brzycki");
   var epley_1rm = get1RM(weight, reps, "epley");
   var lander_1rm = get1RM(weight, reps, "lander");
-
-  for (i = 0; i < repsdata.data.reps.length; i++) {
-    repsdata.data.reps[i][1]=(brzycki_1rm / brzycki_c[i]).toFixed(0);
-    repsdata.data.reps[i][2]=(epley_1rm / epley_c[i]).toFixed(0);
-    repsdata.data.reps[i][3]=(lander_1rm / lander_c[i]).toFixed(0);
-  }
-
-  for (i = 0; i < repsdata.data.percentages.length; i++) {
-    repsdata.data.percentages[i][1]=(brzycki_1rm * percents[i]/100).toFixed(0);
-    repsdata.data.percentages[i][2]=(epley_1rm * percents[i]/100).toFixed(0);
-    repsdata.data.percentages[i][3]=(lander_1rm * percents[i]/100).toFixed(0);
-  }
   
+  repsdata.data.reps.splice(0);
+  repsdata.data.percentages.splice(0);
+
+  for (i = 0; i < 10; i++) {
+    reps1=i+1;
+    reps2=(brzycki_1rm / brzycki_c[i]).toFixed(0);
+    reps3=(epley_1rm / epley_c[i]).toFixed(0);
+    reps4=(lander_1rm / lander_c[i]).toFixed(0);
+    
+    perc1=percents[i].toFixed(0)+'%';
+    perc2=(brzycki_1rm * percents[i]/100).toFixed(0);
+    perc3=(epley_1rm * percents[i]/100).toFixed(0);
+    perc4=(lander_1rm * percents[i]/100).toFixed(0);
+
+    if (nsca != 'No NSCA calculation') {
+      reps5=(get1RM(weight, reps, nsca) / benchCoefficients[i]).toFixed(0);
+      perc5=(get1RM(weight, reps, nsca) * percents[i]/100).toFixed(0);
+      
+      repsdata.data.reps.push([reps1,reps2,reps3,reps4, reps5]);
+      repsdata.data.percentages.push([perc1,perc2,perc3,perc4, perc5]);
+    } 
+    else {
+      repsdata.data.reps.push([reps1,reps2,reps3,reps4]);
+      repsdata.data.percentages.push([perc1,perc2,perc3,perc4]);
+    } 
+  }
+
+  // Adjust labels
+  if (nsca != 'No NSCA calculation') {
+    repsdata.data.replabels[4] = nsca.toString();
+    repsdata.data.percentlabels[4] = nsca.toString();
+  }
+  else {
+    repsdata.data.replabels.splice(4);
+    repsdata.data.percentlabels.splice(4);
+  }
 }
 
 var chartdata = {
   // A labels array that can contain any sort of values
   labels: [10,9,8,7,6,5,4,3,2,1],
   // Our series array that contains series objects or in this case series data arrays
-  series: [
-
-  ]
+  series: []
 };
 
 var chart = null;
@@ -135,7 +145,7 @@ function updateCalculations() {
   if ( reps < 1 && typeof r !== 'number' )
     return;
   
-  updateData(weight, reps);
+  updateData(weight, reps, nsca);
 
   // Create the tables with templates
   var source   = $("#reps-template").html();
@@ -196,24 +206,31 @@ function showRepsModal() {
 
 $(document).ready(function(){
 
+  nsca = $('#nsca-selector').val();
+  
   // On document load
   updateCalculations();
-
+  
   // On input value change
-
+  
   $('#repsInput').on('input',function(e){
     updateCalculations();
   });
-
+  
   $('#weightInput').on('input',function(e){
     updateCalculations();
   });
-
+  
+  $('#nsca-selector').change(function() {
+    nsca=$(this).val();
+    updateCalculations();
+  });
+  
   // Button click handlers
   
-  $('#increase-weight-button').on('click',function(e){ increaseWeight(); updateCalculations(); });
-  $('#decrease-weight-button').on('click',function(e){ decreaseWeight(); updateCalculations(); });
-  $('#increase-reps-button').on('click',function(e){ increaseReps(); updateCalculations(); });
-  $('#decrease-reps-button').on('click',function(e){ decreaseReps(); updateCalculations(); });
+  $('#increase-weight-button').on('click',function(e){ increaseWeight(); });
+  $('#decrease-weight-button').on('click',function(e){ decreaseWeight(); });
+  $('#increase-reps-button').on('click',function(e){ increaseReps(); });
+  $('#decrease-reps-button').on('click',function(e){ decreaseReps(); });
 
 });
